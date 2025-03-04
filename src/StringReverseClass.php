898981f43bb46007e7c42fa7fe7eb8d919a28f80
passionate-laravel-dev <190306264+passionate-laravel-dev@users.noreply.github.com>
2025-03-04 05:04:03 +0000

    Fix styling

--- src/StringReverseClass.php
+++ src/StringReverseClass.php
@@ -9,18 +9,20 @@ class StringReverseClass
         return new static($str);
     }
 
-    public function __construct(protected string $str) {}
+    public function __construct(protected string $str)
+    {
+    }
 
     /**
      * Reverse string
-     * 
+     *
      * @return string
      */
     public function reverse(): string
     {
         $length = strlen($this->str);
 
-        for ($i = 0; $i < $length /  2; $i++) {
+        for ($i = 0; $i < $length / 2; $i++) {
             $temp = $this->str[$i];
             $this->str[$i] = $this->str[$length - $i - 1];
             $this->str[$length - $i - 1] = $temp;
